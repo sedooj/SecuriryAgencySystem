a38a9d3bda536a7bcc8f699d483c17b40f530323
sedooj <sergo54ru@gmail.com>
2025-03-30 19:08:08 +0700

    Refactor Passport model: convert properties to constructor parameters and initialize with default values.

--- Core/Model/Passport.cs
+++ Core/Model/Passport.cs
@@ -1,13 +1,21 @@
 namespace Core.Model;
 
-public class Passport
+public class Passport(
+    string passportNumber,
+    string passportSeries,
+    DateTime issueDate,
+    string surname,
+    string name,
+    string patronymic,
+    string gender,
+    string nationality)
 {
-    public string PassportNumber { get; set; }
-    public string PassportSeries { get; set; }
-    public DateTime IssueDate { get; set; }
-    public string Surname { get; set; }
-    public string Name { get; set; }
-    public string Patronymic { get; set; }
-    public string Gender { get; set; }
-    public string Nationality { get; set; }
+    public string PassportNumber { get; set; } = passportNumber;
+    public string PassportSeries { get; set; } = passportSeries;
+    public DateTime IssueDate { get; set; } = issueDate;
+    public string Surname { get; set; } = surname;
+    public string Name { get; set; } = name;
+    public string Patronymic { get; set; } = patronymic;
+    public string Gender { get; set; } = gender;
+    public string Nationality { get; set; } = nationality;
 }
