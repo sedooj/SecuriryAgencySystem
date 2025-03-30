85cf55cc05290a6a526dda33ea1e2ec88aa230ae
sedooj <sergo54ru@gmail.com>
2025-03-30 20:10:02 +0700

    Add MainMenu page: implement layout and button click handlers for navigation

---
+++ SAS/Pages/MainMenu.xaml.cs
@@ -0,0 +1,34 @@
+namespace SAS.Pages;
+
+public partial class MainMenu : ContentPage
+{
+    public MainMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void OnEmployeesButtonClicked(object sender, EventArgs e)
+    {
+        // Handle Employees button click
+    }
+
+    private void OnClientsButtonClicked(object sender, EventArgs e)
+    {
+        // Handle Clients button click
+    }
+
+    private void OnDutiesButtonClicked(object sender, EventArgs e)
+    {
+        // Handle Duties button click
+    }
+
+    private void OnReportsButtonClicked(object sender, EventArgs e)
+    {
+        // Handle Reports button click
+    }
+
+    private void OnSettingsButtonClicked(object sender, EventArgs e)
+    {
+        // Handle Settings button click
+    }
+}
