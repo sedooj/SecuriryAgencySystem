b0e7e1b6ab861d88eb6c7186e3d2ece8354c6d77
sedooj <sergo54ru@gmail.com>
2025-03-30 19:11:21 +0700

    Fix exception class definitions: remove parentheses from exception class declarations for proper syntax.

--- Core/Exception/AppExceptions.cs
+++ Core/Exception/AppExceptions.cs
@@ -2,5 +2,5 @@ namespace Core.Exception;
 
 using System;
 
-public class OnlySecurityOfficerCanHaveSpecialEquipmentException() : Exception;
-public class SecurityEquipmentNullException() : Exception;
+public class OnlySecurityOfficerCanHaveSpecialEquipmentException : Exception;
+public class SecurityEquipmentNullException : Exception;
